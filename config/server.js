65a08ea775dac01ffa627e96e343e44b09120073
Fambirachimwe <t.svaku@gmail.com>
2021-05-05 11:55:19 +0200

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d204a7d03829b688ab8c252cb34792e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3846f13f-be2d-4949-8a9b-83190e41919a'
+};
